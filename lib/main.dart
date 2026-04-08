4bcacbf31ff9cdf8ec37c7c202660796fb64400e
FibreCase <fibrecase@163.com>
2026-04-08 19:22:14 +0800

    Add Windows runner support with console output and DPI awareness
    - Introduced a manifest file for the Windows runner to enable DPI awareness.
    - Implemented console creation and output redirection in utils.cpp.
    - Added utility functions for command line argument handling and UTF-16 to UTF-8 conversion.
    - Developed Win32Window class for managing high DPI-aware windows, including theme updates and message handling.
    - Created header files for utility functions and Win32Window class.

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_personal_website/pages/home.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FibreCase Profile',
+      theme: ThemeData(
+        colorScheme: .fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
